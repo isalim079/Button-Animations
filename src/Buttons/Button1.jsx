5667eb76ba7f12efde1d682e579abf79214f993c
isalim079 <salimreza1025@gmail.com>
2025-05-24 09:05:21 +0600

    button 5 animation on hover created

--- src/Buttons/Button1.jsx
+++ src/Buttons/Button1.jsx
@@ -1,4 +1,9 @@
+
+import Button5 from "./Button5";
+
 const Button1 = () => {
+
+
   return (
     <div>
       {/* button 1 */}
@@ -15,19 +20,22 @@ const Button1 = () => {
         </button>
       </div>
 
-      {/* button 2 */}
+      {/* button 3 */}
       <div className="mt-10 relative p-1 animate-gradient rounded-full w-fit overflow-hidden">
         <button className=" text-white text-lg px-6 py-3 rounded-full bg-blue-950 ">
           Animation 3
         </button>
       </div>
 
-      {/* button 2 */}
-      <div className="mt-10 relative p-1 animationBtn4 rounded-full w-fit overflow-hidden">
-        <button className=" text-white text-lg px-6 py-3 rounded-full bg-blue-950 ">
+      {/* button 4 */}
+      <div className="mt-10 relative p-1 animationBtn4 rounded-full w-fit overflow-hidden hover:scale-[105%] transition-all duration-200 ease-in-out">
+        <button className=" text-white text-lg px-6 py-3 rounded-full cursor-pointer bg-gray-500 hover:bg-blue-950 transition-all duration-200 ease-in-out ">
           Hover me 4
         </button>
       </div>
+
+      {/* button 5 */}
+     <Button5 />
     </div>
   );
 };
---
+++ src/Buttons/Button5.jsx
@@ -0,0 +1,94 @@
+import { Sparkle } from "lucide-react";
+import { useState } from "react";
+
+const Button5 = () => {
+  const [sparkle3Active, setSparkle3Active] = useState(false);
+  const [sparkle2Active, setSparkle2Active] = useState(false);
+  const [sparkle1Active, setSparkle1Active] = useState(false);
+
+  const handleMouseEnter = () => {
+    // Reset all states first
+    setSparkle3Active(false);
+    setSparkle2Active(false);
+    setSparkle1Active(false);
+
+    // Delay chain animation
+    setTimeout(() => {
+      setSparkle3Active(true);
+      setTimeout(() => {
+        setSparkle3Active(false);
+        setSparkle2Active(true);
+        setTimeout(() => {
+          setSparkle2Active(false);
+          setSparkle1Active(true);
+          setTimeout(() => {
+            setSparkle1Active(false);
+          }, 200); // Sparkle 1 reset
+        }, 200); // Sparkle 2 duration
+      }, 200); // Sparkle 3 duration
+    }, 200); // Initial delay before Sparkle 3
+  };
+
+  return (
+    <div
+      onMouseEnter={() => {
+        handleMouseEnter();
+      }}
+      className="mt-10 relative p-1 animationBtn4 rounded-full w-fit overflow-hidden hover:scale-[105%] transition-all duration-200 ease-in-out group"
+    >
+      <button className=" text-white text-lg px-6 py-4 rounded-full cursor-pointer bg-gray-500 hover:bg-blue-950 transition-all duration-200 ease-in-out ">
+        <span className="flex items-center gap-2">
+          <span className="flex">
+            {/* sparkle 1 */}
+            <span
+              className="-mr-[12px] transition-all duration-200 ease-in-out"
+              style={{
+                transform: sparkle1Active ? "scale(1.4)" : "scale(1)",
+                transition: "all 200ms ease-in-out",
+              }}
+            >
+              <Sparkle
+                strokeWidth={0}
+                size={10}
+                fill={sparkle1Active ? "white" : "#A9A9A9"}
+              />
+            </span>
+
+            {/* sparkle 2 */}
+            <span
+              className="mt-[14px] -mr-[6px] transition-all duration-200 ease-in-out"
+              style={{
+                transform: sparkle2Active ? "scale(1.2)" : "scale(1)",
+                transition: "all 200ms ease-in-out",
+              }}
+            >
+              <Sparkle
+                strokeWidth={0}
+                size={15}
+                fill={sparkle2Active ? "white" : "#A9A9A9"}
+              />
+            </span>
+
+            {/* sparkle 3 */}
+            <span
+              className="transition-all duration-200 ease-in-out"
+              style={{
+                transform: sparkle3Active ? "scale(0.8)" : "scale(1)",
+                transition: "all 200ms ease-in-out",
+              }}
+            >
+              <Sparkle
+                strokeWidth={0}
+                size={24}
+                fill={sparkle3Active ? "#A9A9A9" : "white"}
+              />
+            </span>
+          </span>
+          <span className=""> Hover me 4</span>
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default Button5;
